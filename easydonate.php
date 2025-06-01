aff8dd7583a040ead0b8dd144554158843d5e741
Amodeus89 <guru1989bk@mail.ru>
2025-06-01 13:18:15 +0300

    Delete easydonate.php

--- easydonate.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Plugin Name: EasyDonate Integration
- * Description: Интеграция с EasyDonate API для отображения товаров, топ-донатеров и прогресса сбора на сайте WordPress. Поддержка нескольких магазинов.
- * Version: 1.8
- * Author: BlackMagma
- */
-// Содержимое опущено для краткости
